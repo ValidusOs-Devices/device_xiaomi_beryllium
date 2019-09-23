6bc4db93f69881c40115d584bd26260af6d1d4c7
GtrCraft <nickvanbokhorst@hotmail.com>
2019-09-23 11:34:36 +0200

    beryllium: GZOSP Bringup
    *Deps will be added later

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_beryllium.mk
+    $(LOCAL_DIR)/gzosp_beryllium.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_beryllium-user \
-    lineage_beryllium-userdebug \
-    lineage_beryllium-eng
+    gzosp_beryllium-user \
+    gzosp_beryllium-userdebug \
+    gzosp_beryllium-eng
